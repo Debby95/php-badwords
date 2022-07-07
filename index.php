156c4f55e6fb81aea5550bb358f6b3519ed12233
Debby95 <debbyjacksonstar@hotmail.it>
2022-07-07 14:32:38 +0200

    stampo p

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    $text ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, libero provident molestiae eos suscipit quos, recusandae dicta odio quisquam voluptatem atque, veniam ducimus fugiat voluptates nemo corrupti nisi qui delectus!";
+
+    echo $text;
+?>
+
