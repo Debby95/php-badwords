fc0239d514d105d86e74170b7691ee99a7dbe541
Debby95 <debbyjacksonstar@hotmail.it>
2022-07-07 15:22:28 +0200

    censura la badword

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php
     $text ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, libero provident molestiae eos suscipit quos, recusandae dicta odio quisquam voluptatem atque, veniam ducimus fugiat voluptates nemo corrupti nisi qui delectus!";
+    $badwords = $_GET["badwords"];
 
     echo $text;
     echo "<br>";
     echo "Lunghezza paragrafo: " .strlen($text);
+    
+    
+    var_dump($_GET);
+    echo "Parola da censurare <strong>ipsum</strong>" .stripos($badwords, "***");
+    echo "<br>";
+
+    if (stripos($badwords, "ipsum") !== false) {
+        $badwords = str_replace("ipsum", "***", $text);
+    }
+
+    echo $badwords;
 ?>
+
+
 
 
 
