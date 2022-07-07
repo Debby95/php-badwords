dc28267a3bf90b0fa80b140da5c7e3495cfe5cb7
Debby95 <debbyjacksonstar@hotmail.it>
2022-07-07 14:36:30 +0200

    stampo lunghezza paragrafo

--- index.php
+++ index.php
@@ -2,5 +2,9 @@
     $text ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, libero provident molestiae eos suscipit quos, recusandae dicta odio quisquam voluptatem atque, veniam ducimus fugiat voluptates nemo corrupti nisi qui delectus!";
 
     echo $text;
+    echo "<br>";
+    echo "Lunghezza paragrafo: " .strlen($text);
 ?>
+
+
 
